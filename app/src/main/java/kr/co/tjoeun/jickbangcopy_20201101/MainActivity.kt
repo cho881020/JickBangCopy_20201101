984f966976da57a12e3a6983a734c46857fc8cc9
조경진 <kj_cho@idealidea.co.kr>
2020-11-01 14:13:19 +0900

    액티비티에 방목록담는ArrayList + 실제 방 데이터 채워주기

--- app/src/main/java/kr/co/tjoeun/jickbangcopy_20201101/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20201101/MainActivity.kt
@@ -1,10 +1,11 @@
 package kr.co.tjoeun.jickbangcopy_20201101
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import kr.co.tjoeun.jickbangcopy_20201101.datas.Room
 
 class MainActivity : BaseActivity() {
+
+    val mRooms = ArrayList<Room>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +19,18 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mRooms.add(Room(8500, "서울시 강남구", -2, "서울시 강남구의 지하2층 방입니다."))
+        mRooms.add(Room(8500, "서울시 은평구", 5, "은평구의 5층 방 입니다."))
+        mRooms.add(Room(78000, "서울시 은평구", 3, "은평구의 3층 방 입니다."))
+        mRooms.add(Room(2400, "서울시 은평구", 0, "은평구의 반지하 방 입니다."))
+        mRooms.add(Room(23500, "서울시 서대문구", -1, "서대문구의 지하 1층 방 입니다."))
+        mRooms.add(Room(175000, "서울시 서대문구", 4, "서대문구의 4층 방 입니다."))
+        mRooms.add(Room(55000, "서울시 서대문구", 7, "서대문구의 7층 방 입니다."))
+        mRooms.add(Room(9600, "서울시 동대문구", 0, "동대문구의 반지하 방 입니다."))
+        mRooms.add(Room(38000, "서울시 동대문구", 2, "동대문구의 2층 방 입니다."))
+        mRooms.add(Room(57000, "서울시 동대문구", -2, "동대문구의 지하 2층 방 입니다."))
+        mRooms.add(Room(85000, "서울시 동대문구", 5, "동대문구의 5층 방 입니다."))
 
     }
 

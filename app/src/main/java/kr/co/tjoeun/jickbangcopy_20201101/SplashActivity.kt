09c69756a83c5f6cd16776139d1792ace75420aa
조경진 <kj_cho@idealidea.co.kr>
2020-11-14 10:58:54 +0900

    2.5초 후에 실행

--- app/src/main/java/kr/co/tjoeun/jickbangcopy_20201101/SplashActivity.kt
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20201101/SplashActivity.kt
@@ -1,7 +1,10 @@
 package kr.co.tjoeun.jickbangcopy_20201101
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : BaseActivity() {
 
@@ -17,6 +20,18 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        로딩이 끝나면 (2.5초 정도 후에) => 메인화면으로 이동 => 로딩화면 종료.
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+//            myHandler에게 이 중괄호 내부의 일을 해달라고 요청(할일을 등록 - post)
+            val myIntent = Intent(mContext, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        }, 2500)
+
+
 
     }
 

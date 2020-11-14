5df8e117ceda42ab064f477115f51903703e8676
조경진 <kj_cho@idealidea.co.kr>
2020-11-14 10:42:47 +0900

    스플래쉬(로딩)화면 추가 + 앱의 시작점으로 사용

---
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20201101/SplashActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.tjoeun.jickbangcopy_20201101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
